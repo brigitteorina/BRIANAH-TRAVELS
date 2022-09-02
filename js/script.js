3942f870c38f42cb2c47741a176d469c155760df
Brigitte Orina <brigitteorina@gmail.com>
2022-09-02 08:59:44 +0300

    js debugging

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-alert("Nitamanage")
-//fetching my data
-//function fetchPackage(){
-    fetch("http://localhost:3000/packages")
-    .then(res=>res.json())
-    .then(resp=>{console.log(resp)})
-    .then((data)=>console.log(data))
-//}
-fetchPackage()
-
-
+alert("nitamanage");
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 let registrationForm=document.querySelector('.registration-form-container')
@@ -19,44 +9,62 @@ let loginFormClose = document.querySelector('#login-Close');
 menu.onclick = () =>{
    menu.classList.toggle('fa-times');
    navbar.classList.toggle('active');
-}
+};
 
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+   registrationForm.classList.remove('fa-times');
+   loginForm.classList.remove('active');
+};
 register.addEventListener('click' ,() =>{
-    registrationForm.classList.add('active')
+   registrationForm.classList.add('active')
+})
+registerFormClose.addEventListener('click' ,() => {
+   registrationForm.classList.remove('active')
  })
- registerFormClose.addEventListener('click' ,() => {
-    registrationForm.classList.remove('active')
-  })
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+loginFormClose.addEventListener('click', () =>{
+   loginForm.classList.remove('active');
+   
+});
 
-  var swiper = new Swiper(".home-slider", {
-    loop:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
- });
+var swiper = new Swiper(".reviews-slider", {
+   grabCursor:true,
+   loop:true,
+   autoHeight:true,
+   spaceBetween: 20,
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
 
-  loginFormClose.addEventListener('click', () =>{
-    loginForm.classList.remove('active');
-    
- });
-
- let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
 let currentItem = 3;
-
-
 //apponting the boxes to be displayed
 
 loadMoreBtn.onclick = () =>{
-    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
-    //loop for the boxes
- 
-    for (var i = currentItem; i < currentItem + 3; i++){
-       boxes[i].style.display = 'inline-block';
-    };
+   let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+   //loop for the boxes
 
-
-    //loop 
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   };
+   //loop 
    currentItem += 3;
    if(currentItem >= boxes.length){
       loadMoreBtn.style.display = 'none';
