c55c9a4e8cc3978c622d1ed343edc75bccae98d3
brigitteorina <brigitteorina@gmail.com>
2022-09-20 18:48:49 +0300

    handle form

--- js/script.js
+++ js/script.js
@@ -79,6 +79,7 @@ function packages(){
       let packages = data.packages;
       console.log(packages);
       let packagesContainer = document.querySelector('.home-packages .box-container');
+      packagesContainer.innerHTML = '';
       let packageHTML = '';
       packages.forEach(package => {
          packageHTML += `
@@ -101,32 +102,70 @@ function packages(){
    });
 }
 
-packages();
+// packages();
 
-fetch('./db.json')
-.then(response => response.json())
-.then(data => {
-   let destination = data.destinations;
-   console.log(destination);
-   let destinationContainer = document.querySelector('.packages .box-container');
-   let destinationHTML = '';
-   destination.forEach(destination => {
-      destinationHTML += `
-      <div class="box">
-         <div class="image">
-            <img src="${destination.img_url}" alt="">
+function destinations(){
+   fetch('./db.json')
+   .then(response => response.json())
+   .then(data => {
+      let destination = data.destinations;
+      console.log(destination);
+      let destinationContainer = document.querySelector('.packages .box-container');
+      destinationContainer.innerHTML = '';
+      let destinationHTML = '';
+      destination.forEach(destination => {
+         destinationHTML += `
+         <div class="box">
+            <div class="image">
+               <img src="${destination.img_url}" alt="">
+            </div>
+            <div class="content">
+               <h3>${destination.resort}</h3>
+               <p>
+               <p>${destination.payment}</p>
+               <h3>${destination.country}</h3>
+               ${destination.description}
+               <a href="#" class="btn">Read More</a>
+            </div>
          </div>
-         <div class="content">
-            <h3>${destination.resort}</h3>
-            <p>
-            <p>${destination.payment}</p>
-            <h3>${destination.country}</h3>
-            ${destination.description}
-            <a href="#" class="btn">Read More</a>
-         </div>
-      </div>
-      `;
+         `;
+      });
+      destinationContainer.innerHTML += destinationHTML;
    });
-   destinationContainer.innerHTML += destinationHTML;
-});
+}
 
+document.querySelector('.book-form').addEventListener('submit', (e) => {
+   e.preventDefault();
+   let name = e.target.name.value;
+   let email = e.target.email.value;
+   let phone = e.target.phone.value;
+   let address = e.target.address.value;
+   let location = e.target.location.value;
+   let guests = e.target.guests.value;
+   let dateIn = e.target.arrivals.value;
+   let leaving = e.target.leaving.value;
+   let data = {
+      name,
+      email,
+      phone,
+      address,
+      location,
+      guests,
+      dateIn,
+      leaving
+   }
+   let bookingResponse = `Hello ${name}\n\n`
+   bookingResponse += `Thank you for booking with us.\n\n`
+   bookingResponse += `Your booking details are as follows:\n\n`
+   bookingResponse += `Email: ${email}\n`
+   bookingResponse += `Phone: ${phone}\n`
+   bookingResponse += `Address: ${address}\n`
+   bookingResponse += `Location: ${location}\n`
+   bookingResponse += `Guests: ${guests}\n`
+   bookingResponse += `Arrival Date: ${dateIn}\n`
+   bookingResponse += `Leaving Date: ${leaving}\n\n`
+   bookingResponse += `We will get back to you shortly.\n\n`
+   bookingResponse += `Regards,\n`
+   bookingResponse += `Travel Agency`
+   alert(bookingResponse);
+});
